fe3181d181fab4d46e8f7c43bdd5668120333974
Luana Franca <21898@student.dorset-college.ie>
2020-11-03 15:21:46 +0000

    Adding move, making carpark generic for vehicles

--- src/com/company/Bicycle.java
+++ src/com/company/Bicycle.java
@@ -4,11 +4,19 @@ public class Bicycle extends Cycle {
 
     public Bicycle() {
         MessageCentre.DebugMessage("Creating a Bicycle");
+
+        setManufacturer("");
+        setModelName("");
     }
 
     @Override
     public String toString() {
 
         return "I am a bicycle";
+    }
+
+    @Override
+    public void Move(int speed, String direction) {
+
     }
 }

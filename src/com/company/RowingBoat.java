fc637561ba5a02d3414aa541eb0622a22531cd19
Luana Franca <21898@student.dorset-college.ie>
2020-11-03 15:13:21 +0000

    Added carpark, collections and boat

--- src/com/company/RowingBoat.java
+++ src/com/company/RowingBoat.java
@@ -1,9 +1,16 @@
 package com.company;
 
-public class RowingBoat extends Vehicle {
+public class RowingBoat extends Boat {
 
     public RowingBoat() {
 
         MessageCentre.DebugMessage("Creating a Rowing Boat");
+    }
+
+
+    @Override
+    public String toString() {
+
+        return String.format("rowing boat");
     }
 }
